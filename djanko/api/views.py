cf70e1afdd6cadb044abcd4d5ddbacdac70df88e
orichcasperkevin <orichcaspertheghost@gmail.com>
2023-10-26 15:14:02 +0300

    add authorization to view

--- djanko/api/views.py
+++ djanko/api/views.py
@@ -1,22 +1,49 @@
 import datetime
 from django.contrib.auth import get_user_model
+from django.http import JsonResponse
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.generics import GenericAPIView
 from rest_framework import status
 
 from .serializers import UserSerializer
+from ..hanko import authenticate
+
 
 User = get_user_model()
+
+def deny(reason):
+    return JsonResponse(
+        {"error": "Unauthorized " + str(reason)},
+        safe=False,status=401
+    )
+
+def extract_token_from_header(header: str) -> str:
+    parts = header.split()
+    return parts[1] if len(parts) == 2 and parts[0].lower() == "bearer" else None
 
 
 class CreateUser(GenericAPIView):
     serializer_class = UserSerializer
 
-    def post(self, request):
-        data = request.data
-        hanko_id = "suiu-wejnkwne-sdjh"    
-        serializer = self.get_serializer(data=data)
+    def post(self, request):       
+        authorization = request.headers.get("Authorization")
+        if not authorization:
+            return deny("'Authorization' header was not found")
+
+        token = extract_token_from_header(authorization)               
+        if not token:
+            return deny('No token')
+
+        valid,token_data = authenticate(token)
+        if not valid:
+            return deny(token_data)           
+        if valid:
+            if not token_data:
+                deny("No data in token")            
+        
+        hanko_id = token_data.get('sub')
+        serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         user = serializer.save(hanko_id=hanko_id)
         return Response({
